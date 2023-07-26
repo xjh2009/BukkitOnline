bf898e90de41cdc87a60539c77b1a7bdfa45624d
bert_18 <bert_18@qq.com>
2023-07-26 19:48:37 +0800

    0.1

---
+++ src/main/java/top/wutzu/bukkitonline/BukkitOnline.java
@@ -0,0 +1,51 @@
+package top.wutzu.bukkitonline;
+
+import com.sun.net.httpserver.HttpServer;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.logging.Logger;
+
+public final class BukkitOnline extends JavaPlugin {
+    public static FileConfiguration config;
+    public static HttpServer httpServer;
+    public static Logger logger;
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("BukkitOnline插件加载中");
+        config = getConfig();
+        logger = getLogger();
+        getLogger().info("加载配置中");
+        if (!new File("./plugins/BukkitOnline/config.yml").exists()){
+            getLogger().info("未检测到配置文件，将加载默认配置");
+            this.saveDefaultConfig();
+        }
+        getLogger().info("加载配置完成");
+        new Thread(new BukkitRunnable(){
+            @Override
+            public void run(){
+                try {
+                    httpServer = HttpServer.create(new InetSocketAddress(39058),100);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                httpServer.createContext("/",new Index());
+                httpServer.start();
+            }
+        }).start();
+        getLogger().info("开启http服务成功，端口39058");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info("卸载BukkitOnline中");
+        httpServer.stop(3);
+        getLogger().info("http服务已关闭");
+    }
+}
---
+++ src/main/java/top/wutzu/bukkitonline/Index.java
@@ -0,0 +1,147 @@
+package top.wutzu.bukkitonline;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginLogger;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class Index implements HttpHandler {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String playerListString = "";
+        BukkitOnline.logger.info(httpExchange.getRemoteAddress().getHostName() + httpExchange.getRequestHeaders().getFirst("User-Agent"));
+        if(Bukkit.getServer().getOnlinePlayers().size()!=0){
+            for (Player player: Bukkit.getServer().getOnlinePlayers()){
+                playerListString = playerListString + player.getDisplayName() + "<br>";
+            }
+        }
+        double playerPer = (double) Bukkit.getOnlinePlayers().size() / Bukkit.getMaxPlayers();
+        String page = "\n" +
+                "<!-- saved from url=(0016)https://seir.in/ -->\n" +
+                "<html lang=\"zh-CN\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
+                "    \n" +
+                "    <title>{ServerName}</title>\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no\">\n" +
+                "    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/gh/AyagawaSeirin/homepage@latest/mdui/css/mdui.min.css\">\n" +
+                "    <link rel=\"stylesheet\" href=\"https://v.06dn.com/api/v3/slave/download/0/L3VwbG9hZHMvMTU3NzUvMjAyMzA3LzI2LzE1Nzc1X3BTM09BODMyX21haW4uY3Nz/main.css?sign=oLUZFoSZqoe_vI_ExTSYzBKgGiQ8Dzzy04O-WIFd82E%3D%3A1690456360\">\n" +
+                "    <link rel=\"stylesheet\" href=\"https://seir.in/assets/css/index.css\">\n" +
+                "    <!-- <link rel=\"stylesheet\" href=\"/assets/css/detaile.css\"> -->\n" +
+                "    <link rel=\"stylesheet\" href=\"https://at.alicdn.com/t/font_1625701_q2422cy34wn.css\">\n" +
+                "    <link rel=\"icon\" href=\"https://seir.in/assets/img/logo-circul-100.png\">\n" +
+                "    <!-- Global site tag (gtag.js) - Google Analytics -->\n" +
+                "    <script type=\"text/javascript\" async=\"\" src=\"https://www.google-analytics.com/analytics.js\"></script>\n" +
+                "    <script type=\"text/javascript\" async=\"\" src=\"https://www.googletagmanager.com/gtag/js?id=G-363RTB0YDC&amp;l=dataLayer&amp;cx=c\"></script>\n" +
+                "    <script async=\"\" src=\"https://www.googletagmanager.com/gtag/js?id=UA-162013750-5\"></script>\n" +
+                "\n" +
+                "</head>\n" +
+                "<body class=\"mdui-theme-primary-pink mdui-theme-accent-pink mdui-loaded\">\n" +
+                "<div id=\"rin-bg\"></div>\n" +
+                "<main id=\"rin-main\">\n" +
+                "    <div class=\"mdui-row\">\n" +
+                "        \n" +
+                "        <div class=\"mdui-col-xs-12 mdui-col-md-10\">\n" +
+                "            <div class=\"mdui-card rin-card\">\n" +
+                "<!--                <div class=\"rin-card-title\">关于我</div>-->\n" +
+                "                <div class=\"rin-card-part\" id=\"introduce\" style=\"margin-top: 0;\">\n" +
+                "                    <div class=\"rin-card-content mdui-typo\">\n" +
+                "                        {ServerDescription}\n" +
+                "                    </div>\n" +
+                "                </div>\n" +
+                "                <!--        <div class=\"rin-tr\"></div>-->\n" +
+                "                <div class=\"rin-card-part\" id=\"fighting\">\n" +
+                "                    <div class=\"rin-card-title2 mdui-typo\"><a href=\"https://seir.in/#fighting\">#</a>在线玩家</div>\n" +
+                "                    <div class=\"mdui-container\">\n" +
+                "                        <div class=\"mdui-row\">\n" +
+                "                            <div class=\"mdui-col-xs-12 mdui-col-sm-6 mdui-col-md-4\" mdui-tooltip=\"{content: &#39;在线玩家数&#39;}\">\n" +
+                "\n" +
+                "                                <div class=\"rin-progress-title\">"+ Bukkit.getOnlinePlayers().size()+"/"+Bukkit.getMaxPlayers()+"</div>\n" +
+                "\n" +
+                "                                <div class=\"mdui-progress\">\n" +
+                "                                    <div class=\"mdui-progress-determinate\" style=\"width: "+playerPer * 100+"%;\"></div>\n" +
+                "                                </div>\n" +
+                "\n" +
+                "                            </div>\n" +
+                "\n" +
+                "                            \n" +
+                "\n" +
+                "                            \n" +
+                "\n" +
+                "                        </div>\n" +
+                "\n" +
+                "                        \n" +
+                "\n" +
+                "                        \n" +
+                "\n" +
+                "\n" +
+                "                    </div>\n" +
+                "                </div>\n" +
+                "                <div class=\"rin-card-part\" id=\"contact\">\n" +
+                "                    <div class=\"rin-card-content mdui-typo\">"+playerListString+"</div>\n" +
+                "\n" +
+                "                    \n" +
+                "\n" +
+                "                    \n" +
+                "\n" +
+                "                    \n" +
+                "\n" +
+                "                    \n" +
+                "\n" +
+                "                </div>\n" +
+                "\n" +
+                "                \n" +
+                "\n" +
+                "\n" +
+                "                <div class=\"rin-card-part\" id=\"works\">\n" +
+                "                    <div class=\"rin-card-title2 mdui-typo\"><a href=\"https://seir.in/#works\">#</a>TPS</div><div class=\"rin-card-title2 mdui-typo\">"+ Arrays.toString(Bukkit.getServer().getTPS()) +"</div>\n" +
+                "\n" +
+                "                    <a target=\"_blank\" href=\"https://github.com/AyagawaSeirin/UploadGithubForTypecho\">\n" +
+                "                        \n" +
+                "                    </a>\n" +
+                "                    \n" +
+                "                    \n" +
+                "                    \n" +
+                "\n" +
+                "                </div>\n" +
+                "\n" +
+                "\n" +
+                "                <div class=\"rin-tr\"></div>\n" +
+                "\n" +
+                "                <div class=\"rin-card-part\" id=\"last\">\n" +
+                "                    <div class=\"rin-card-title2\">{ServerName}</div>\n" +
+                "                    <div class=\"rin-card-content\">{Sentence}</div>\n" +
+                "                </div>\n" +
+                "\n" +
+                "                <div class=\"rin-card-part\" id=\"footer\">\n" +
+                "                    <div class=\"rin-card-content\">©2023 WutzuBert | Made with love by Seirin</div>\n" +
+                "                </div>\n" +
+                "            </div>\n" +
+                "        </div>\n" +
+                "    </div>\n" +
+                "</main>\n" +
+                "<script src=\"https://cdn.jsdelivr.net/gh/AyagawaSeirin/homepage@latest/mdui/js/mdui.min.js\"></script>\n" +
+                "<!--<script src=\"/assets/js/main.js\"></script>-->\n" +
+                "\n" +
+                "\n" +
+                "</body></html>";
+        String serverName = BukkitOnline.config.getString("ServerName");
+        String serverDescription = BukkitOnline.config.getString("ServerDescription");
+        String sentence = BukkitOnline.config.getString("Sentence");
+        page = page.replace("{ServerName}",serverName);
+        page = page.replace("{ServerDescription}",serverDescription);
+        page = page.replace("{Sentence}",sentence);
+        httpExchange.sendResponseHeaders(200,page.getBytes(StandardCharsets.UTF_8).length);
+        httpExchange.getResponseHeaders().set("Content-Type","text/html; charset=utf8");
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(page.getBytes(StandardCharsets.UTF_8));
+        os.close();
+    }
+}
